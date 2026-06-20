504ba66407e1f3d762e09a6f5acef0df91bd4c00
Aaron Bittel <aaron@bittels.de>
2026-06-20 14:59:30 +0200

    feat: status - untracked, modified

--- status.go
+++ status.go
@@ -4,12 +4,30 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type StatusCmd struct {
 	description string
 
 	fs *flag.FlagSet
+}
+
+type repoStatus struct {
+	untracked  []string
+	unmodified []string
+	modified   []string
+}
+
+func newRepoStatus() *repoStatus {
+	return &repoStatus{
+		untracked:  []string{},
+		unmodified: []string{},
+		modified:   []string{},
+	}
 }
 
 func NewStatusCmd(fs *flag.FlagSet) *StatusCmd {
@@ -41,5 +59,76 @@ func (cmd *StatusCmd) Run(w io.Writer, args ...string) error {
 		return err
 	}
 
+	gitRepo, err := findGitRepo()
+	if err != nil {
+		return err
+	}
+
+	repoStatus, err := status(gitRepo)
+	if err != nil {
+		return err
+	}
+
+	if len(repoStatus.modified) > 0 {
+		fmt.Fprintln(w, "modified:")
+		for _, path := range repoStatus.modified {
+			fmt.Fprintf(w, "  %s\n", path)
+		}
+	}
+
+	if len(repoStatus.untracked) > 0 {
+		fmt.Fprintln(w, "untracked:")
+		for _, path := range repoStatus.untracked {
+			fmt.Fprintf(w, "  %s\n", path)
+		}
+	}
+
 	return nil
+}
+
+func status(gitRepo string) (*repoStatus, error) {
+	indexFile := filepath.Join(gitRepo, ".git", "index")
+	data, err := os.ReadFile(indexFile)
+	if err != nil {
+		return nil, err
+	}
+
+	entries, err := readIndex(data)
+	if err != nil {
+		return nil, err
+	}
+
+	repoStatus := newRepoStatus()
+
+	filepath.WalkDir(gitRepo, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if strings.Contains(path, ".git") {
+			return nil
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		p, err := filepath.Rel(gitRepo, path)
+		if err != nil {
+			return err
+		}
+
+		for _, entry := range entries {
+			if entry.path == p {
+				repoStatus.unmodified = append(repoStatus.unmodified, p)
+				return nil
+			}
+		}
+
+		repoStatus.untracked = append(repoStatus.untracked, p)
+
+		return nil
+	})
+
+	return repoStatus, nil
 }
