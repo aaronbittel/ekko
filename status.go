2c34d4e8ee1f94c47c7734efbb397f0ad9601803
Aaron Bittel <aaron@bittels.de>
2026-06-21 16:22:14 +0200

    feat: ekko-status modified vs unmodified

--- status.go
+++ status.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
 	"io"
@@ -8,6 +10,12 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"syscall"
+)
+
+const (
+	RED   = "\033[38;2;190;81;81m"
+	RESET = "\033[0m"
 )
 
 type StatusCmd struct {
@@ -64,43 +72,60 @@ func (cmd *StatusCmd) Run(w io.Writer, args ...string) error {
 		return err
 	}
 
-	repoStatus, err := status(gitRepo)
+	indexFile := filepath.Join(gitRepo, ".git", "index")
+	data, err := os.ReadFile(indexFile)
+	if err != nil {
+		return err
+	}
+
+	entries, err := readIndex(data)
+	if err != nil {
+		return err
+	}
+
+	ignoredDirs := []string{}
+	f, err := os.Open(".gitignore")
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	defer f.Close()
+	ignoredDirs = readGitignoreFile(f)
+
+	repoStatus, err := status(entries, gitRepo, filepath.WalkDir, ignoredDirs)
 	if err != nil {
 		return err
 	}
 
 	if len(repoStatus.modified) > 0 {
-		fmt.Fprintln(w, "modified:")
+		fmt.Fprintln(w, "Changes not staged for commit:")
 		for _, path := range repoStatus.modified {
-			fmt.Fprintf(w, "  %s\n", path)
+			msg := fmt.Sprintf("%smodified:   %s%s", RED, path, RESET)
+			fmt.Fprintf(w, "\t%s\n", msg)
 		}
+		fmt.Fprintln(w)
 	}
 
 	if len(repoStatus.untracked) > 0 {
-		fmt.Fprintln(w, "untracked:")
+		fmt.Fprintln(w, "Untracked files:")
+		fmt.Fprintf(w, "\t")
 		for _, path := range repoStatus.untracked {
-			fmt.Fprintf(w, "  %s\n", path)
+			msg := fmt.Sprintf("%s%s%s   ", RED, path, RESET)
+			fmt.Fprint(w, msg)
 		}
+		fmt.Fprintln(w)
+	} else {
+		fmt.Fprintln(w, "no changes added to commit")
 	}
 
 	return nil
 }
 
-func status(gitRepo string) (*repoStatus, error) {
-	indexFile := filepath.Join(gitRepo, ".git", "index")
-	data, err := os.ReadFile(indexFile)
-	if err != nil {
-		return nil, err
-	}
+type walkFunc func(root string, fn fs.WalkDirFunc) error
 
-	entries, err := readIndex(data)
-	if err != nil {
-		return nil, err
-	}
-
+func status(entries []*indexEntry, root string, walk walkFunc, ignoredDirs []string) (*repoStatus, error) {
 	repoStatus := newRepoStatus()
 
-	filepath.WalkDir(gitRepo, func(path string, d fs.DirEntry, err error) error {
+	walk(root, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
@@ -113,14 +138,28 @@ func status(gitRepo string) (*repoStatus, error) {
 			return nil
 		}
 
-		p, err := filepath.Rel(gitRepo, path)
+		p, err := filepath.Rel(root, path)
 		if err != nil {
 			return err
 		}
 
+		for _, dir := range ignoredDirs {
+			if strings.HasPrefix(p, dir) {
+				return nil
+			}
+		}
+
 		for _, entry := range entries {
 			if entry.path == p {
-				repoStatus.unmodified = append(repoStatus.unmodified, p)
+				modified, err := isModified(p, entry)
+				if err != nil {
+					return err
+				}
+				if modified {
+					repoStatus.modified = append(repoStatus.modified, p)
+				} else {
+					repoStatus.unmodified = append(repoStatus.unmodified, p)
+				}
 				return nil
 			}
 		}
@@ -131,4 +170,61 @@ func status(gitRepo string) (*repoStatus, error) {
 	})
 
 	return repoStatus, nil
+}
+
+func isModified(path string, entry *indexEntry) (bool, error) {
+	stat, err := getFileStat(path)
+	if err != nil {
+		return false, err
+	}
+
+	if sameModificationTime(stat, entry) {
+		return false, nil
+	}
+
+	if !sameSize(stat, entry) {
+		return true, nil
+	}
+
+	f, err := os.Open(path)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	ok, err := sameComputeHash(path, f, entry)
+	if err != nil {
+		return false, err
+	}
+
+	return !ok, nil
+}
+
+func sameModificationTime(stat *syscall.Stat_t, entry *indexEntry) bool {
+	return stat.Mtim.Sec == int64(entry.mtime.seconds) && stat.Mtim.Nsec == int64(entry.mtime.nanoseconds)
+}
+
+func sameComputeHash(path string, r io.Reader, entry *indexEntry) (bool, error) {
+	pHash, err := computeObjectHash(path, typeBlob, r)
+	if err != nil {
+		return false, err
+	}
+	return bytes.Equal(entry.gitSha[:], pHash[:]), nil
+}
+
+func sameSize(stat *syscall.Stat_t, entry *indexEntry) bool {
+	return stat.Size == int64(entry.fileSize)
+}
+
+func readGitignoreFile(r io.Reader) []string {
+	lines := []string{}
+	scanner := bufio.NewScanner(r)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+		lines = append(lines, line)
+	}
+	return lines
 }
